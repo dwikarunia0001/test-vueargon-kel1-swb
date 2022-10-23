4d64ef9f14b3ceb65734c1e36d5dbd11bc7e470e
Aisah <78716604+Aisah-08@users.noreply.github.com>
2022-10-23 13:26:31 +0700

    menambah logout

--- src/examples/Sidenav/SidenavList.vue
+++ src/examples/Sidenav/SidenavList.vue
@@ -1,139 +1,103 @@
 <template>
-  <div
-    class="collapse navbar-collapse w-auto h-auto h-100"
-    id="sidenav-collapse-main"
-  >
+  <div class="collapse navbar-collapse w-auto h-auto h-100" id="sidenav-collapse-main">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <sidenav-item
-          url="/dashboard"
-          :class="getRoute() === 'dashboard' ? 'active' : ''"
-          :navText="this.$store.state.isRTL ? 'لوحة القيادة' : 'Dashboard'"
-        >
+        <sidenav-item url="/dashboard" :class="getRoute() === 'dashboard' ? 'active' : ''" :navText="this.$store.state.isRTL ? 'لوحة القيادة' : 'Dashboard'">
           <template v-slot:icon>
             <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
           </template>
         </sidenav-item>
       </li>
       <li class="nav-item">
-        <sidenav-item
-          url="/dashboard/todo"
-          :class="getRoute() === 'todo' ? 'active' : ''"
-          :navText="this.$store.state.isRTL ? 'حة القيادة' : 'Todo'"
-        >
+        <sidenav-item url="/dashboard/tables" :class="getRoute() === 'tables' ? 'active' : ''" :navText="this.$store.state.isRTL ? 'الجداول' : 'Tables'">
           <template v-slot:icon>
-            <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
+            <i class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i>
           </template>
         </sidenav-item>
       </li>
       <li class="nav-item">
-        <sidenav-item
-          url="/dashboard/tables"
-          :class="getRoute() === 'tables' ? 'active' : ''"
-          :navText="this.$store.state.isRTL ? 'الجداول' : 'Tables'"
-        >
-          <template v-slot:icon>
-            <i
-              class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"
-            ></i>
-          </template>
-        </sidenav-item>
-      </li>
-      <li class="nav-item">
-        <sidenav-item
-          url="/dashboard/billing"
-          :class="getRoute() === 'billing' ? 'active' : ''"
-          :navText="this.$store.state.isRTL ? 'الفواتیر' : 'Billing'"
-        >
+        <sidenav-item url="/dashboard/billing" :class="getRoute() === 'billing' ? 'active' : ''" :navText="this.$store.state.isRTL ? 'الفواتیر' : 'Billing'">
           <template v-slot:icon>
             <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
           </template>
         </sidenav-item>
       </li>
       <li class="mt-3 nav-item">
-        <h6
-          v-if="this.$store.state.isRTL"
-          class="text-xs ps-4 text-uppercase font-weight-bolder opacity-6"
-          :class="this.$store.state.isRTL ? 'me-4' : 'ms-2'"
-        >
-          صفحات المرافق
-        </h6>
-        <h6
-          v-else
-          class="text-xs ps-4 text-uppercase font-weight-bolder opacity-6"
-          :class="this.$store.state.isRTL ? 'me-4' : 'ms-2'"
-        >
-          ACCOUNT PAGES
-        </h6>
+        <h6 v-if="this.$store.state.isRTL" class="text-xs ps-4 text-uppercase font-weight-bolder opacity-6" :class="this.$store.state.isRTL ? 'me-4' : 'ms-2'">صفحات المرافق</h6>
+        <h6 v-else class="text-xs ps-4 text-uppercase font-weight-bolder opacity-6" :class="this.$store.state.isRTL ? 'me-4' : 'ms-2'">ACCOUNT PAGES</h6>
       </li>
       <li class="nav-item">
-        <sidenav-item
-          url="/dashboard/profile"
-          :class="getRoute() === 'profile' ? 'active' : ''"
-          :navText="this.$store.state.isRTL ? 'حساب تعريفي' : 'Profile'"
-        >
+        <sidenav-item url="/dashboard/profile" :class="getRoute() === 'profile' ? 'active' : ''" :navText="this.$store.state.isRTL ? 'حساب تعريفي' : 'Profile'">
           <template v-slot:icon>
             <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
           </template>
         </sidenav-item>
       </li>
       <li class="nav-item">
-        <sidenav-item
-          url="/auth/signin"
-          :class="getRoute() === 'signin' ? 'active' : ''"
-          :navText="this.$store.state.isRTL ? 'تسجيل الدخول' : 'Sign In'"
-        >
+        <sidenav-item url="/auth/signin" :class="getRoute() === 'signin' ? 'active' : ''" :navText="this.$store.state.isRTL ? 'تسجيل الدخول' : 'Sign In'">
           <template v-slot:icon>
             <i class="ni ni-single-copy-04 text-danger text-sm opacity-10"></i>
           </template>
         </sidenav-item>
       </li>
       <li class="nav-item">
-        <sidenav-item
-          url="/auth/signup"
-          :class="getRoute() === 'signup' ? 'active' : ''"
-          :navText="this.$store.state.isRTL ? 'اشتراك' : 'Sign Up'"
-        >
+        <sidenav-item url="/auth/signup" :class="getRoute() === 'signup' ? 'active' : ''" :navText="this.$store.state.isRTL ? 'اشتراك' : 'Sign Up'">
           <template v-slot:icon>
             <i class="ni ni-collection text-info text-sm opacity-10"></i>
           </template>
         </sidenav-item>
       </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link" @click="logout">
+          <div class="icon icon-shape icon-sm text-center d-flex align-items-center justify-content-center">
+            <i class="ni ni-button-power text-warning text-sm opacity-10"></i>
+          </div>
+          <span class="nav-link-text ms-1 text-warning">Sign Out</span>
+        </a>
+      </li>
     </ul>
   </div>
   <div class="pt-3 mx-3 mt-3 sidenav-footer">
-    <sidenav-card
-      :class="cardBg"
-      textPrimary="Need Help?"
-      textSecondary="Please check our docs"
-    />
+    <sidenav-card :class="cardBg" textPrimary="Need Help?" textSecondary="Please check our docs" />
   </div>
 </template>
 <script>
 import SidenavItem from "./SidenavItem.vue";
 import SidenavCard from "./SidenavCard.vue";
+import { mapActions } from "pinia";
+import d$auth from "@/stores/auth";
 
 export default {
   name: "SidenavList",
   props: {
-    cardBg: String
+    cardBg: String,
   },
   data() {
     return {
       title: "Argon Dashboard 2",
       controls: "dashboardsExamples",
-      isActive: "active"
+      isActive: "active",
     };
   },
   components: {
     SidenavItem,
-    SidenavCard
+    SidenavCard,
   },
   methods: {
     getRoute() {
       const routeArr = this.$route.path.split("/");
       return routeArr[1];
-    }
-  }
+    },
+    ...mapActions(d$auth, ["a$logout"]),
+    logout() {
+      try {
+        this.a$logout();
+        alert("Logout Successfully");
+        this.$router.replace({ name: "Signin" });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
 };
 </script>
--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,43 +1,50 @@
 import { defineStore } from "pinia";
-import * as s$auth from '@/services/auth';
-import { certCookies, setCookies } from '@/plugins/cookies';
+import * as s$auth from "@/services/auth";
+import { certCookies, setCookies } from "@/plugins/cookies";
 
 const d$auth = defineStore({
-    id: 'auth',
-    state: () => ({
-        id: undefined,
-        name: undefined,
-        role: undefined,
-    }),
-    actions: {
-        async a$setUser() {
-            try {
-                const { id, name, role } = certCookies();
-                this.id = id;
-                this.name = name;
-                this.role = role;
-                return 'User Authenticated!';
-            } catch ({ message }) {
-                this.id = undefined;
-                this.name = undefined;
-                this.role = undefined;
-                throw message;
-            }
-        },
-        async a$login(body) {
-            try {
-                const { data } = await s$auth.login(body);
-                setCookies('CERT', data.token, { datetime: data.expiresAt });
-                this.a$setUser();
-                return true;
-            } catch ({ error, message }) {
-                throw message ?? error;
-            }
-        }
+  id: "auth",
+  state: () => ({
+    id: undefined,
+    name: undefined,
+    role: undefined,
+  }),
+  actions: {
+    async a$setUser() {
+      try {
+        const { id, name, role } = certCookies();
+        this.id = id;
+        this.name = name;
+        this.role = role;
+        return "User Authenticated!";
+      } catch ({ message }) {
+        this.id = undefined;
+        this.name = undefined;
+        this.role = undefined;
+        throw message;
+      }
     },
-    getters: {
-        g$user: ({ id, name, role }) => ({ id, name, role }),
+    async a$login(body) {
+      try {
+        const { data } = await s$auth.login(body);
+        setCookies("CERT", data.token, { datetime: data.expiresAt });
+        this.a$setUser();
+        return true;
+      } catch ({ error, message }) {
+        throw message ?? error;
+      }
     },
-})
+    async a$logout() {
+      try {
+        delCookies("CERT");
+      } catch ({ error, message }) {
+        throw message ?? error;
+      }
+    },
+  },
+  getters: {
+    g$user: ({ id, name, role }) => ({ id, name, role }),
+  },
+});
 
 export default d$auth;
